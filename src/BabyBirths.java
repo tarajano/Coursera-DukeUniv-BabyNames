02fa6f8acae25554a71ac2df03f4f9edf58ca1a9
Manuel A. Alonso-Tarajano <tarajano@gmail.com>
2016-10-27 00:26:09 -0400

    Modified totalBirths() to count the number of names in the file.

--- src/BabyBirths.java
+++ src/BabyBirths.java
@@ -14,15 +14,19 @@ public class BabyBirths {
 		}
 	}
 	public static void totalBirths(FileResource input_file) {
-		int boys = 0;
-		int girls = 0;
+		int boys, girls, boys_names, girls_names;
+		boys = girls = boys_names = girls_names = 0;
 		for(CSVRecord rec : input_file.getCSVParser(false)){
-			if(rec.get(1).equals("M"))
+			if(rec.get(1).equals("M")){
 				boys += Integer.parseInt(rec.get(2));
-			else if(rec.get(1).equals("F"))
+				boys_names += 1;
+			}else if(rec.get(1).equals("F")){
 				girls += Integer.parseInt(rec.get(2));
+				girls_names += 1;
+			}
 		}
-		System.out.println("Total:" + (boys + girls) +  " Girls:" + girls + " Boys:" + boys);
+		System.out.println("TotalBirths:" + (boys + girls) +  " Girls:" + girls + " Boys:" + boys);
+		System.out.println("TotalNames:" + (boys_names + girls_names) +  " GirlsNames:" + girls_names + " BoysNames:" + boys_names);
 	}
 	public static void testTotalBirths(){
 		totalBirths(new FileResource("us_babynames/us_babynames_test/example-small.csv"));
